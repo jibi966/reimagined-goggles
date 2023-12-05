5eafd1d188463db4511af8f6a53c36215df4ef38
Jibin <jibin@empplan.com>
2023-12-05 20:13:47 +0530

    layout with nvmrc

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.json());
+
+/**
+ * routes here
+ */
+
+app.get("*", (req, res) => {
+  return res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+app.listen(PORT, async () => {
+  try {
+    // await db here
+    console.log("Server listening on port", PORT);
+  } catch (error) {
+    console.log("Error Connecting", error);
+  }
+});
